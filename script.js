5443957c375ba8715061cb35e321715af374133c
Matt Sch <matthys.schaffner@outlook.fr>
2024-10-24 14:58:29 +0200

    premier commit

---
+++ script.js
@@ -0,0 +1,125 @@
+// Exercice 1 :
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// Exercice 2 :
+// for(let i = 0; i<=10; i++){
+//     let x;
+//     console.log(x = x+i);
+// }
+
+// Exercice 3 :
+// const a = 5;
+// const b = 8;
+// let c = (a*2 + b*2);
+// console.log("le perimetre est de " + c); 
+
+// Exercice 4 :
+// const x = 15;
+// if( x % 3 == 0 || x % 5 ==0 ){
+//     if( x % 3 == 0 && x % 5 == 0){
+//         console.log( x + " est multiple de 3 et de 5")
+//     }else if( x % 3 == 0){
+//         console.log( x + " est multiple de 3")
+//     }else{
+//         console.log( x + " est multiple de 5")
+//     }
+// }else{
+//     console.log( x + " est ni multiple de 3, ni de 5")
+// }
+
+// Exercice 5 :
+// const a = 56;
+// const b = 78;
+// const c = 45;
+// console.log("Le plus grand est " + Math.max(a,b,c));
+
+// Exercice 6 :
+const x = 29;
+if(x >= 1){
+    if( let i = 2; i/Math.sqrt(i); i++){
+        
+    }
+} else{
+    console.log( x + " doit être un nombre superieur à 1 pour être un nombre premier.")
+}
+
+// Exercice 7 :
+// let x = 1;
+// while( x <= 30){
+//     if( x % 3 == 0){
+//         console.log( x + " est un multiple de 3")
+//     }
+//     x++;
+// }
+
+// Exercice 8 :
+// const x = 98765;
+// console.log(x.toString().length);
+
+// Exercice 9 :
+// const x = 12345; 
+// const addition = x
+//     .toString()
+//     .split('')
+//     .map(Number)
+//     .reduce((acc, val) => acc + val, 0);
+// console.log(addition);
+
+// Exercice 10 :
+// const x = 375;
+// console.log( x + " secondes vaut " + Math.floor(x / 60) + " minutes et " + x%60 +" secondes")
+
+// Exercice 11 :
+// const x = 12345;
+// console.log(Number(x.toString().split("").reverse().join("")))
+
+// Exercice 12 :
+// let i =1;
+// while (i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+// Exercice 13 :
+// let a =5;
+// let b = "5";
+// b = Number(b);
+// if(a == b){
+//     console.log(a + " et " + b + " sont égaux");
+// } else {
+//     console.log("Les nombres sont différents");
+// }
+
+// Exercice 14 : 
+// let number = 10;
+// if (number % 2 === 0){
+//     console.log(number + " est pair");
+// } else if (number % 2 !== 0){
+//     console.log(number + " est impair");
+// }
+// if ( number % 2 === 0 ? console.log(number + " est pair") : console.log(number + " est impair"));
+
+// Exercice 15 :
+// for (let i = 1; i<= 20; i++){
+//     if (i %2 === 0 ? console.log(i) : " ");
+// }
+
+// Exercice 16 : 
+// let numbers = [10, 20, 30, 40, 50];
+// for (let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i]);
+// }
+
+// Exercice 17 :
+// let color = "vert";
+// if (color === "rouge"){
+//     console.log("la couleur est rouge")
+// } else if (color === "bleue"){
+//     console.log("la couleur est bleue")
+// } else if (color === "jaune"){
+//     console.log("la couleur est jaune")
+// } else{
+//     console.log("la couleur n'est pas reconnue")
+// }
